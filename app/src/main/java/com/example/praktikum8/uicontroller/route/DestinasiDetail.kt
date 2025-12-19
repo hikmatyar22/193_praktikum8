32177db5fdf9af83ba995817604a78b94cc63625
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-19 10:38:23 +0700

    membuat route dengan beberapa destinasi seperti detail,entry, dan home sebagai route dan title dan sebagai tujuan navigasi.

---
+++ app/src/main/java/com/example/praktikum8/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,8 @@
+package com.example.praktikum8.uicontroller.route
+
+import com.example.praktikum8.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_detail"
+    override val titleRes = R.string.detail_siswa
+}
---
+++ app/src/main/java/com/example/praktikum8/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum8.uicontroller.route
+
+import com.example.praktikum8.R
+
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
