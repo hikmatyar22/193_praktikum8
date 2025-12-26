a991001d1d73390d7f48ff3d384667a78ca87d38
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-26 18:15:39 +0700

    mengupdate entryview model Kode iniuntuk digunakan untuk mengelola input penambahan data siswa dengan melakukan validasi, memperbarui state UI, dan menyimpan data ke repository jika seluruh input sudah valid.

--- app/src/main/java/com/example/praktikum8/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/EntryViewModel.kt
@@ -11,50 +11,34 @@ import com.example.praktikum8.modeldata.toDetailSiswa
 import com.example.praktikum8.repositori.RepositoryDataSiswa
 import retrofit2.Response
 
-class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) :
+class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa):
     ViewModel() {
-
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-    /* Fungsi untuk memvalidasi input */
-    private fun validasiInput(
-        uiState: DetailSiswa = uiStateSiswa.detailSiswa
-    ): Boolean {
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):
+            Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
 
-    //Fungsi untuk menangani saat ada perubahan pada text input
     fun updateUiState(detailSiswa: DetailSiswa) {
         uiStateSiswa =
-            UIStateSiswa(
-                detailSiswa = detailSiswa,
-                isEntryValid = validasiInput(detailSiswa)
-            )
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 
-    /* Fungsi untuk menyimpan data yang di-entry */
     suspend fun addSiswa() {
-        if (!validasiInput()) return
-
-        try {
-            val response = repositoryDataSiswa.postDataSiswa(
-                uiStateSiswa.detailSiswa.toDataSiswa()
+        if (validasiInput()) {
+            val sip: Response<Void> = repositoryDataSiswa.postDataSiswa(
+                uiStateSiswa
+                    .detailSiswa.toDataSiswa()
             )
-
-            if (response.isSuccessful) {
-                println("✅ Sukses Tambah Data")
+            if (sip.isSuccessful) {
+                println("Sukses tambah data: ${sip.message()}")
             } else {
-                println("❌ Gagal tambah data: ${response.code()}")
+                println("Gagal tambah data:${sip.errorBody()} ")
             }
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            println("🔥 ERROR addSiswa: ${e.message}")
         }
     }
-
-
 }
