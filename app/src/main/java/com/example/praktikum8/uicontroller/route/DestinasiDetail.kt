1b2f37a1a10d1c07bf9fb2f70f586750b65ca7f9
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-26 18:58:08 +0700

    mengupdate destinasiDetail dengan menambahkan itemIdArg

--- app/src/main/java/com/example/praktikum8/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/praktikum8/uicontroller/route/DestinasiDetail.kt
@@ -2,7 +2,10 @@ package com.example.praktikum8.uicontroller.route
 
 import com.example.praktikum8.R
 
-object DestinasiDetail : DestinasiNavigasi {
-    override val route = "item_detail"
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
