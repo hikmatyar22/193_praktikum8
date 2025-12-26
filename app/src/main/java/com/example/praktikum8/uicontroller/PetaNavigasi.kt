fca6e4d765f9ac3bcf0483b1528b8f6d9d3b79a7
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-26 19:01:56 +0700

    menambahkan code di peta navigasi dengan kode terbaru menambahkan navigasi ke halaman Detail dan Edit siswa dengan pengiriman parameter ID menggunakan NavType.IntType, sedangkan kode lama hanya mendukung navigasi Home dan Entry tanpa fitur melihat detail maupun mengedit data siswa.

--- app/src/main/java/com/example/praktikum8/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/praktikum8/uicontroller/PetaNavigasi.kt
@@ -3,14 +3,20 @@ package com.example.praktikum8.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.praktikum8.uicontroller.route.DestinasiEntry
 import com.example.praktikum8.uicontroller.route.DestinasiHome
 import com.example.praktikum8.uicontroller.route.DestinasiDetail
+import com.example.praktikum8.uicontroller.route.DestinasiEdit
+import com.example.praktikum8.view.DetailSiswaScreen
+import com.example.praktikum8.view.EditSiswaScreen
 import com.example.praktikum8.view.EntrySiswaScreen
 import com.example.praktikum8.view.HomeScreen
+
 
 @Composable
 fun DataSiswaApp(
@@ -44,6 +50,23 @@ fun HostNavigasi(
                     navController.navigate(DestinasiHome.route)
                 }
             )
+        }
+        composable(
+            DestinasiDetail.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiDetail.itemIdArg){
+                type = NavType.IntType
+            })) {
+            DetailSiswaScreen(
+                navigateToEditItem = {navController.navigate("${DestinasiEdit.route}/$it")},
+                navigateBack = {navController.navigate(DestinasiHome.route)})
+        }
+        composable(
+            DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit
+                .itemIdArg){
+                type = NavType.IntType
+            })) {
+            EditSiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)},
+                onNavigateUp = {navController.navigateUp()})
         }
     }
 }
